191f001cef00800636139c4862a40b44e9d40249
Zhican Yang <coolyangzc@qq.com>
2021-07-26 11:58:12 +0800

    Add UE4 project, UE4CLR_Test, and pass the test

---
+++ UE4CLR_Test/Source/UE4CLR_Test/UE4CLR_Test.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4CLR_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4CLR_Test, "UE4CLR_Test" );
